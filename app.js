996512cf474c6f8dab2fe48e0a6bdc2fdfefed4a
승희 <bangseunghee1108@gmail.com>
2023-12-29 12:33:31 +0900

    server 파일 작성

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
-// const path = require("path"); 
 
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/public'));
+// 정적 파일들을 제공하기 위한 미들웨어 설정
+app.use(express.static(path.join(__dirname, 'public')));
 
+// 루트 경로에 대한 요청 처리
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+  // index.html 파일의 경로를 제대로 설정
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
- });
+});
